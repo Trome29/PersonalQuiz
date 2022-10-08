4b46226795fde16d8964228dcca8ed893bbb1385
Trome29 <108098922+Trome29@users.noreply.github.com>
2022-10-08 18:47:42 +0200

    create ui elements

---
+++ PersonalQuiz/ViewControllers/QuestionsViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  PersonalQuiz
+//
+//  Created by Roman on 04.10.2022.
+//
+
+import UIKit
+
+class QuestionsViewController: UIViewController {
+    
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var questionProgressView: UIProgressView!
+    
+    @IBOutlet var singleStackView: UIStackView!
+    @IBOutlet var singleButtons: [UIButton]!
+    
+    @IBOutlet var multipleStackView: UIStackView!
+    @IBOutlet var multipleLabels: [UILabel]!
+    @IBOutlet var multipleSwitches: [UISwitch]!
+    
+    @IBOutlet var rangedStackView: UIStackView!
+    @IBOutlet var rangedSlider: UISlider!
+    @IBOutlet var rangedLabels: [UILabel]!
+    
+    
+    private let questions = Question.getQuestions()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+    
+    
+    @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func multipleAnswerButtonPressed() {
+    }
+    
+    @IBAction func rangedAnswerButtonPressed() {
+    }
+}
+
+// MARK: - Private Methods
+extension QuestionsViewController {
+    private func updateUI() {
+        for stackView in [singleStackView, multipleStackView, rangedStackView] {
+            stackView?.isHidden.toggle()
+        }
+    }
+}
