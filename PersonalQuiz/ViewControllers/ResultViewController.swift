3bfe6f76d9e63ee6076e752932283acd78fc6b3b
Trome29 <108098922+Trome29@users.noreply.github.com>
2022-10-09 01:17:04 +0200

    logic implementation

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -9,16 +9,13 @@ import UIKit
 
 class ResultViewController: UIViewController {
     
-    @IBOutlet var animalImageLabel: UILabel!
+    @IBOutlet var animalTypeLabel: UILabel!
     @IBOutlet var animalDescriptionLabel: UILabel!
-    
     
     var answers: [Answer]!
     var animals: [Character] = []
-    var counter = 0
     
-    let animalStringImage = "Вы - "
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.hidesBackButton = true
@@ -26,33 +23,16 @@ class ResultViewController: UIViewController {
         calculateResult()
     }
     
-    func calculateResult() {
-        answers.forEach { animals.append($0.animal.rawValue) }
-        print(animals)
-        
-        let dogs = animals.filter{ $0 == "🐶" }
-        let cats = animals.filter{ $0 == "🐱" }
-        let rabbits = animals.filter{ $0 == "🐰" }
-        let turtles = animals.filter{ $0 == "🐢" }
-        
-        counter = dogs.count
-        if cats.count > counter {
-            counter = cats.count
-        } else if rabbits.count > counter {
-            counter = rabbits.count
-            
-        } else if turtles.count > counter {
-            counter = turtles.count
-            
-        } else {
-            
-        }
-        print(counter)
-        
-      
-    }
-    
     @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
         navigationController?.dismiss(animated: true)
+    }
+}
+
+// MARK: - Private Methods
+extension ResultViewController {
+    
+    private func calculateResult() {
+        answers.forEach { animals.append($0.animal.rawValue) }
+        
     }
 }
